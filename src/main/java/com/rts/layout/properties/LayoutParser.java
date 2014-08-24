b1290563807bacfcef938f07c5073785a1acc448
Pavan Kumar Mankala <pavan.mankala@gmail.com>
2014-08-24 21:22:38 +0200

    fix layout parsing

--- src/main/java/com/rts/layout/properties/LayoutParser.java
+++ src/main/java/com/rts/layout/properties/LayoutParser.java
@@ -11,7 +11,7 @@ import java.util.regex.Matcher;
 
 /**
  * @author p.mankala
- *
+ * 
  */
 public class LayoutParser implements GenericValueParser<Layout> {
     @Override
@@ -39,9 +39,24 @@ public class LayoutParser implements GenericValueParser<Layout> {
             layoutRows[row] = layoutRowsList.get(row);
         }
 
-        LayoutArea[][] totalArea = new LayoutArea[layoutRows.length][columns];
+        char[][] transpose = new char[columns][layoutRows.length];
 
-        LayoutArea currArea = null;
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < layoutRows.length; j++) {
+                char c = layoutRows[j][i];
+                switch (c) {
+                case '-':
+                    c = '|';
+                    break;
+                case '|':
+                    c = '-';
+                    break;
+                }
+                transpose[i][j] = c;
+            }
+        }
+
+        LayoutArea[][] totalArea = new LayoutArea[layoutRows.length][columns];
 
         char[] lastRow = layoutRows[layoutRows.length - 1];
 
@@ -53,6 +68,7 @@ public class LayoutParser implements GenericValueParser<Layout> {
 
         // Row-wise
         for (int row = 0; row < layoutRows.length; row++) {
+            LayoutArea currArea = null;
             char[] currLayoutRow = layoutRows[row];
             boolean useNewArea = false;
 
@@ -73,25 +89,13 @@ public class LayoutParser implements GenericValueParser<Layout> {
                 totalArea[row][col] = currArea;
             }
         }
-
-        // Column-wise
-        char[][] transpose = new char[columns][layoutRows.length];
-
-        for (int i = 0; i< columns ; i++) {
-            for (int j = layoutRows.length - 1, tCol = 0; j > -1; j--, tCol++) {
-                char c = layoutRows[j][i];
-                switch (c) {
-                    case '-':
-                        c = '|';
-                        break;
-                    case '|':
-                        c = '-';
-                        break;
-                }
-                transpose[i][tCol] = c;
+        for (int i = 0; i < totalArea.length; i++) {
+            for (int j = 0 ; j < columns; j++) {
+                System.out.print(totalArea[i][j]);
             }
+            System.out.println();
         }
-
+        // Column-wise
         lastRow = transpose[transpose.length - 1];
 
         for (int i = 0; i < lastRow.length; i++) {
@@ -100,26 +104,33 @@ public class LayoutParser implements GenericValueParser<Layout> {
             }
         }
 
-        for (int row = 0; row < transpose.length; row++) {
+        for (int row = 1; row < transpose.length; row++) {
             char[] currLayoutRow = transpose[row];
-            boolean useNewArea = false;
+            boolean useExisting = false;
 
-            if (currLayoutRow[columns - 1] == '%') {
-                currLayoutRow[columns - 1] = '|';
+            if (currLayoutRow[currLayoutRow.length - 1] == '%') {
+                currLayoutRow[currLayoutRow.length - 1] = '|';
             }
 
             for (int col = 0; col < currLayoutRow.length; col++) {
                 if (currLayoutRow[col] == '%' && currLayoutRow[col + 1] == '-') {
-                    totalArea[row][col] = currArea = new LayoutArea();
-                    useNewArea = true;
+                    useExisting = true;
                     continue;
                 } else if (currLayoutRow[col] == '%' && currLayoutRow[col + 1] != '-') {
-                    useNewArea = false;
+                    useExisting = false;
                 }
 
-                currArea = ((row == 0 || useNewArea) ? currArea : totalArea[row - 1][col]);
-                totalArea[row][col] = currArea;
+                if (!useExisting) {
+                    totalArea[col][row] = totalArea[col][row - 1];
+                }
             }
+        }
+
+        for (int i = 0; i < totalArea.length; i++) {
+            for (int j = 0 ; j < columns; j++) {
+                System.out.print(totalArea[i][j]);
+            }
+            System.out.println();
         }
 
         return null;
